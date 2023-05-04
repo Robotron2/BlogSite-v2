296a3bb93dc23485cea1d8d4c5b707db1562a4cc
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-05-04 08:49:05 +0100

    Created blogSchema and compose blog route.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 const _ = require("lodash")
 
 const app = express()
@@ -9,6 +10,40 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+
+//Connect to DB
+mongoose.connect("mongodb://localhost:27017/BlogsDB")
+
+const blogSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	author: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String,
+		required: true
+	}
+})
+
+const defaultBlog = `
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima iste quam fuga tempora voluptate cupiditate veniam est, similique nisi, quo blanditiis? Dignissimos magni esse ab et possimus fuga recusandae praesentium?
+Enim distinctio officia quas, tenetur porro exercitationem, qui quibusdam ad accusamus nemo provident repudiandae dolorum id vero veritatis odit quis voluptates error sequi. Ad veritatis eligendi maxime eos officia aut?
+Numquam soluta laborum odio inventore natus, voluptates, distinctio aliquid veniam voluptatum animi laudantium commodi ex minus exercitationem ipsum dignissimos unde id aperiam, quos rerum quae tempora cum. Eius, excepturi aliquid?
+`
+
+const Blog = mongoose.model("Blog", blogSchema)
+
+app.get("/", function (req, res) {
+	res.render("home", { fact: defaultBlog })
+})
+
+app.get("/compose", function (req, res) {
+	res.render("compose")
+})
 
 //Spin up the server
 app.listen(4000, () => {
